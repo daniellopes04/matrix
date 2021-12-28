9becf872bd19472a035260261aa863726c0315b2
Daniel Lopes <daniel.lopes04@gmail.com>
2021-12-28 12:01:27 -0300

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -9,7 +9,7 @@ let charArr = [
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
 ]
 
-let maxCharCount = 100
+let maxCharCount = 300
 let fallingCharArr = []
 let fontSize = 12
 let maxColumns = canvasWidth / fontSize
